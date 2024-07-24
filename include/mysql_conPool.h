27f71ab88440df6d1b1600d352f707a6be61851e
swt <728682458@qq.com>
2024-07-24 15:19:05 +0800

    123

--- include/mysql_conPool.h
+++ include/mysql_conPool.h
@@ -1,5 +1,25 @@
 //数据库连接池
 #pragma once
 #include<iostream>
+#include<string>
 #include<mysql/mysql.h>
+#include<queue>
+
+class Mysql_conPool
+{
+public:
+
+
+
+private:
+    //连接池用队列实现
+    queue<MYSQL *> conPool;
+
+    //成员变量有：url port user password databaseName
+    string m_url;
+    string m_port;
+    string m_user;
+    string m_password;
+    string m_databaseName;
+};
 
